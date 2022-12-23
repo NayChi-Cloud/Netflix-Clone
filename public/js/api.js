35f668bc9dcee84ecc9999c2ca7b5897c529ccd8
NayChi-Cloud <onyxjay1728@gmail.com>
2022-12-23 13:40:58 +0630

    first commit

---
+++ public/js/api.js
@@ -0,0 +1,7 @@
+let api_key = "27c3fb679027693890b6362a717c76a4";
+
+let img_url = "https://image.tmdb.org/t/p/w500";
+let original_img_url = "https://image.tmdb.org/t/p/original";
+let genres_list_http = "https://api.themoviedb.org/3/genre/movie/list?";
+let movie_genres_http = "https://api.themoviedb.org/3/discover/movie?";
+let movie_detail_http = "https://api.themoviedb.org/3/movie";
